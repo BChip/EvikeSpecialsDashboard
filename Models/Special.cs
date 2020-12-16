9705b5e7feda652892ad7e26200d8f48ca9e49fc
unknown <bachippi@svsu.edu>
2020-12-16 02:50:14 -0500

    draft

---
+++ Models/Special.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvikeSpecialSearch.Models
+{
+    public class Special
+    {
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+    }
+}
